2c893763d117493d824bcf6676a927d631376a30
magui <m>
2020-05-31 17:19:40 -0300

    final

--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./styles/Footer.css";
 const FooterBar = () => {
   return (
-    <div className="footer-bar">
-      <a href="#">
-        <img src={require("./img/logo.png")}></img>
-      </a>
-      <span className="copy-text pl-2">All rights reserved Oniblue 2018</span>
+    <div className="footer-bar d-flex align-items-center">
+      <div>
+        <a href="#">
+          <img src={require("./img/logo.png")}></img>
+        </a>
+      </div>
+      <div className="copy-text pl-2">
+        <span>All rights reserved Oniblue 2018</span>
+      </div>
     </div>
   );
 };
